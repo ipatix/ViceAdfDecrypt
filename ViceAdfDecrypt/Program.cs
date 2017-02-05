2db7adb052a2a1531068e287ec83892e3ddac571
ipatix <ipatix@users.noreply.github.com>
2017-02-05 15:49:41 +0100

    Initial Commit

---
+++ ViceAdfDecrypt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ViceAdfDecrypt
+{
+    class Program
+    {
+        static void Usage()
+        {
+            Console.Error.WriteLine("ViceAdfDecrypt <input_radio_station.adf>");
+            Environment.Exit(1);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                    Usage();
+
+                string input = args[0];
+
+                byte[] file_bytes = File.ReadAllBytes(input);
+                for (int i = 0; i < file_bytes.Length; i++)
+                {
+                    file_bytes[i] ^= 0x22;
+                }
+                string output = Path.GetFileNameWithoutExtension(input) + ".mp3";
+                File.WriteAllBytes(output, file_bytes);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("And error has occured:" + Environment.NewLine + e.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ ViceAdfDecrypt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ViceAdfDecrypt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ViceAdfDecrypt")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("afc7cad3-7b78-4d13-a5b2-0fd71dc9aaff")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
